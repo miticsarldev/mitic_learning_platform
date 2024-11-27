bfd83051a574ed82d1eda05bbda86ef03ac661c5
sounkalo-mitic <sounkalo.sidibe@miticsarl.com>
2024-11-27 15:07:28 +0100

    implementation de la page acceuil college

--- src/app/acceuil_college/page.tsx
+++ src/app/acceuil_college/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import Avis from "@/components/college/section/Avis";
 import DomainesEtudes from "@/components/college/section/DomaineEtudes";
-import HeroSection from "@/components/ui/primaire/section/HeroSection";
-import PopularCourses from "@/components/ui/primaire/section/PopularCourse";
+import Navbar from "@/components/navbar";
+import ContactSection from "@/components/ui/contactForm/ContactSection";
+import FounderSection from "@/components/ui/contactForm/FounderSection";
+import FooterSection from "@/components/ui/footer/FooterSection";
+import { Gallery } from "@/components/ui/primaire/components/Gallery";
+import { LandingPage } from "@/components/ui/primaire/section/heroSection/LandingPage";
 
 export default function AcceuilCollege() {
     return (
         <div >
-            <HeroSection />
-            <PopularCourses />
+            <Navbar />
+            <LandingPage />
+            <Gallery />
             <DomainesEtudes />
             <Avis />
+            <FounderSection />
+            <ContactSection />
+            <FooterSection />
         </div>
     );
 }
