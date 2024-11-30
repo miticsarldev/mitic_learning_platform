5fdcccae57b86ea1a9d8c1a83ebe98edc792a251
gitmpd <mohamedpapadiarra@gmail.com>
2024-11-30 19:24:46 +0000

    [ESM-10]  Implémentation de la page profile (vue étudiant ou apprenant)

---
+++ src/app/profil_etudiant/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import ProfilePage from "@/components/college/ProfilPage";
+import Navbar from "@/components/navbar";
+
+export default function Landing() {
+    return (
+        <div >
+            <Navbar />
+            <ProfilePage/>
+        </div>
+    );
+}
